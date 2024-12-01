59b3c5225f8a6152a138767fa4788d66c630ad20
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-12-01 22:48:49 +0700

    fix

--- src/controllers/labsController.js
+++ src/controllers/labsController.js
@@ -1,17 +1,26 @@
+//labsController
 const asyncHandler = require("express-async-handler");
 const { passwordConfig: SQLAuthentication, noPasswordConfig: PasswordlessConfig } = require('../config/config.js');
 const { createDatabaseConnection } = require('../config/database.js');
-const database = createDatabaseConnection(SQLAuthentication);
+
+// Make sure to await the database connection properly
+let database;
+createDatabaseConnection(SQLAuthentication).then((db) => {
+  database = db;
+});
+
 const getAllLabs = asyncHandler(async (req, res) => {
-    try {
-        const labs = await database.readAllLabs();
-        console.log(`labs: ${JSON.stringify(labs)}`);
-        res.status(200).json(labs);
-    } catch (err) {
-        res.status(500).json({ error: err?.message });
-    }
+  try {
+    // Wait for the database connection to be established before calling the method
+    const labs = await database.readAllLabs();
+    console.log(`labs: ${JSON.stringify(labs)}`);
+    res.status(200).json(labs);
+  } catch (err) {
+    res.status(502).json({ error: err?.message });
+  }
 });
 
 module.exports = {
-    getAllLabs
+  getAllLabs
 };
+
